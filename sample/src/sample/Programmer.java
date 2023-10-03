98767c2242ecb51a26cfeadfe18a3976901b9504
Dell <Dell@MunnangiTriveni>
2023-10-03 10:53:52 +0530

    first commit

---
+++ sample/src/sample/Programmer.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Programmer extends Single {
+	int bonus=4000;
+
+	public static void main(String[] args) {
+		Programmer  p = new Programmer();
+		System.out.println("The programmer salary is :"+p.salary);
+	}
+
+}
---
+++ sample/src/sample/Single.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class Single {
+	float salary=30000;
+
+}
